93f8aeba04b0b919e4f7e2967b4605fcb34ad1d1
zwl <1016590863@qq.com>
2023-09-15 17:52:06 +0800

    配置

--- base/getConfig.go
+++ base/getConfig.go
@@ -3,11 +3,15 @@ package base
 import (
 	yaml "gopkg.in/yaml.v2"
 	"io/ioutil"
+	"path"
+	"runtime"
 )
 
 // 获取sdk 配置信息
 func GetConfig() (sdkConfig *SdkConfig, err error) {
-	file, err := ioutil.ReadFile("./config/config.yaml")
+	_, filePath, _, _ := runtime.Caller(0)
+	dirPath := path.Dir(filePath)
+	file, err := ioutil.ReadFile(dirPath + "/../config/config.yaml")
 	if err != nil {
 		return
 	}
